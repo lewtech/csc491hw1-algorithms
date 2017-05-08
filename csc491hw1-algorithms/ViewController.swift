3c576b04cb05cf57d9b294ba25797a4c816c845a
lewtech <lewtech@gmail.com>
2017-05-08 08:20:46 -0500

    Initial Commit

---
+++ csc491hw1-algorithms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  csc491hw1-algorithms
+//
+//  Created by Lew Flauta on 5/8/17.
+//  Copyright © 2017 Lew Flauta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
