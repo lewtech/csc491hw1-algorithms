b9c55849a6fa4ca6ec6046d8eaff13345e639971
lewtech <lewtech@gmail.com>
2017-05-08 09:19:58 -0500

    initial

--- csc491hw1-algorithms/ViewController.swift
+++ csc491hw1-algorithms/ViewController.swift
@@ -9,16 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var A:[Int] = [6,1,4,5,2,6]
 
+    @IBOutlet weak var arrayLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        arrayLabel.text = "Initial Array Here"
+        algSelectionSort(array: A)
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func algSelectionSort(array:Array<Any>){
+        var A:[Int] = array as! [Int]
+
+        self.arrayLabel.text = String(describing: A)
+
+let when = DispatchTime.now() + 2 // change 2 to desired number of seconds
+
+
+
+            var key:Int
+            var i:Int
+            for j:Int in 0...A.count-1{
+                DispatchQueue.main.asyncAfter(deadline: when) {
+                    // Your code with delay
+                }
+
+
+
+                key = A[j]
+                print(A)
+                i = j-1
+                while ((i>=0) && (A[i]>key)){
+                    A[i+1] = A[i]
+                    i = i - 1
+
+                    
+                }
+                A[i+1] = key
+
+            }
+
+
+
+    self.arrayLabel.text = String(describing: A)
+    print (A)
     }
+
+
+
 
 
 }
