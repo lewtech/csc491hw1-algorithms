c4b4234fbb7ed1f785e944761ef12bb4160efef3
lewtech <lewtech@gmail.com>
2017-05-09 21:47:01 -0500

    update got uicollectionviewdelegate up and running

---
+++ csc491hw1-algorithms/BarCell.swift
@@ -0,0 +1,55 @@
+//
+//  BarCell.swift
+//  csc491hw1-algorithms
+//
+//  Created by Lew Flauta on 5/8/17.
+//  Copyright © 2017 Lew Flauta. All rights reserved.
+//
+
+import UIKit
+
+class BarCell: UICollectionViewCell {
+    let barView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    var barHeightConstraint: NSLayoutConstraint?
+
+    override var isHighlighted: Bool {
+        didSet {
+            barView.backgroundColor = isHighlighted ? .black : .red
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(barView)
+
+        //        barView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+
+        barHeightConstraint = barView.heightAnchor.constraint(equalToConstant: 300)
+        barHeightConstraint?.isActive = true
+
+        barHeightConstraint?.constant = 100
+
+        //        barView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+
+        barView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+
+        barView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+
+        barView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+
+
+
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
--- csc491hw1-algorithms/ViewController.swift
+++ csc491hw1-algorithms/ViewController.swift
@@ -8,15 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    var A:[Int] = [6,1,4,5,2,6]
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    var values:[Int] = [6,1,4,5,2,6]
+    let identifier = "CellId"
+    let cellId = "cellId"
 
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var arrayLabel: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.backgroundColor = .gray
         arrayLabel.text = "Initial Array Here"
-        algSelectionSort(array: A)
 
+
+
+        collectionView.dataSource = self
+
+
+
+    }
+
+
+    @IBOutlet weak var processSelectionSort: UIButton!
+
+    @IBAction func startSelectionSort(_ sender: Any) {
+
+        self.arrayLabel.text = String(describing: values)
+        algSelectionSort(array: values)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! BarCell
+
+        if let max = values.max() {
+            let value = values[indexPath.item]
+            let ratio = value / max
+
+            //cell.barHeightConstraint?.constant = maxHeight() * ratio
+        }
+
+        return cell
     }
 
     func algSelectionSort(array:Array<Any>){
@@ -24,18 +61,19 @@ class ViewController: UIViewController {
 
         self.arrayLabel.text = String(describing: A)
 
-let when = DispatchTime.now() + 2 // change 2 to desired number of seconds
+
 
 
 
             var key:Int
             var i:Int
             for j:Int in 0...A.count-1{
-                DispatchQueue.main.asyncAfter(deadline: when) {
-                    // Your code with delay
+
+                sleep(2)
+                DispatchQueue.main.async {
+                    self.arrayLabel.text = String(describing: A)
+                    print (A)
                 }
-
-
 
                 key = A[j]
                 print(A)
@@ -52,13 +90,14 @@ let when = DispatchTime.now() + 2 // change 2 to desired number of seconds
 
 
 
-    self.arrayLabel.text = String(describing: A)
-    print (A)
+
     }
 
+    func maxHeight() -> CGFloat {
+        return view.frame.height - 20 - 44 - 8
+    }
 
-
-
+    func empty(){}
 
 }
 
